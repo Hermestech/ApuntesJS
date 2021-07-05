f6f01a222c402d34fda54e0b20ef7abb1ef7dfd6
Hermes Aguilar <adanhermes23@gmail.com>
2021-07-04 19:33:16 -0500

    modulos

---
+++ BasicoPooJs/module.mjs
@@ -0,0 +1,8 @@
+import { PlatziClass } from "./main.mjs";
+const clase67 = new PlatziClass({
+    name: "Javascript: orientado a objetos basado en prototipos",
+    videoID: "Fjmgil42"
+});
+
+clase67.reproducir();
+clase67.pausar();
