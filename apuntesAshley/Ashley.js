152870a35260b83ccfc364eba541926831d9516b
Hermes Aguilar <adanhermes23@gmail.com>
2021-09-30 22:14:37 -0500

    html a renderizar

---
+++ apuntesAshley/Ashley.js
@@ -0,0 +1,16 @@
+//
+var nombre = ' Hermes';
+
+var numero = 8;
+
+var verdadero = true;
+
+console.log('hola' + nombre)
+
+var lado = 12
+
+console.log('El perimetro del cuadrado es: ', lado * 4)
+
+var a = 2
+var b = 5
+console.log(2*(a+b))
